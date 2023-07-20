208307559b3aeb9dc4a2e76165659df0e037d67e
Miguel Gil F <67928892+Miguel-web@users.noreply.github.com>
2023-07-20 21:51:17 +0000

    fetch ready and useEffect + button delete all

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 const API_URL= "https://fake-todo-list-52f9a4ed80ce.herokuapp.com/todos/user/miguelweb"
 
 const TodoList = () => {
@@ -7,10 +7,25 @@ const TodoList = () => {
     const handleTask = (event) => {
         setTask(event.target.value); 
     } 
-    const addTask = (event) => {
-        if (event.key == 'Enter' && task.trim() !== '') {
-            setListTask([...listTask, task]);
-            setTask('');
+    const addTask = async (event) => {
+        try {
+            if (event.key == 'Enter' && task.trim() !== '') {
+               const newTaskList = [...listTask, {label: task, done: false}];
+               const response = await fetch(API_URL, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newTaskList)
+            })
+                if (response.ok) {
+                    getAllUserTasks()
+                }
+                setTask('');
+            }
+
+        } catch (error) {
+            console.log(error)
         }
     }
     const createUser = async () => {
@@ -22,28 +37,74 @@ const TodoList = () => {
                     "Content-Type": "application/json"
                 }}
             const response = await fetch(API_URL, requestOpt)
-                if (response.status == 201) {
+                if (response.ok) {
                     // aqui llamo las tareas del ursuario
+                    getAllUserTasks()
                 } else if (response.status == 400) {
                     // el usuario existe , puedo llamar las tareas
+                    getAllUserTasks()
                 }
 
         } catch (error) { 
             console.log(error)
         }
     }
-
     const getAllUserTasks = async () => {
             try {
                 const response = await fetch(API_URL)
-                if (response.ok) {}
+                if (response.ok) {
+                const body = await response.json()
+                    setListTask(body)
+                } else if (response.status == 400) {
+                    // el usuarion no existe , puedo crearlo
+                    createUser()
+                }
 
             } catch (error) {
              console.log(error)
             }
     }
+    const deleteUserTask = async (indexid) => {
+        const newTaskList = listTask.filter((task, index)=> (indexid != index))
+        if (newTaskList.length == 0) {
+            deleteAll()
+        } else {
+            
+            try {
+                const response = await fetch(API_URL, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(newTaskList)
+                })
+                if (response.ok) {
+                    getAllUserTasks()
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+    const deleteAll = async () => {
+        try {
+        const response = await fetch(API_URL,{
+                method: "DELETE"
+            })
+            if (response.ok) {
+                getAllUserTasks()
+            } else if (response.status == 404) {
+                getAllUserTasks()
+            }
+            
+        } catch (error) {
+            console.log(error)
+         }
+    }
 
-    const handlerButtomDelete = (indexid) => setListTask(listTask.filter((task, index)=> (indexid != index)))
+useEffect(()=> {
+    getAllUserTasks()
+},[])
 
     return (
         <div className="text-center text-white d-flex align-items-center gap-3 flex-column">
@@ -54,7 +115,7 @@ const TodoList = () => {
                 {
                     listTask.map((task, index) => {
                         return (
-                        <li key={index}> <h3>{task} <button type='button' className='btn btn-close' onClick={(event) => handlerButtomDelete(index)}></button></h3> </li>
+                        <li key={index}> <h3>{task.label} <button type='button' className='btn btn-close' onClick={(event) => deleteUserTask(index)}></button></h3> </li>
                         )
                     })
                 }
@@ -62,11 +123,10 @@ const TodoList = () => {
             </div>
             <div className='m-1'> <p>{listTask.length === 0
               ? "No tasks, (😊Hola Jose...)"
-              : listTask.length + " Item Left👍"}</p></div>    
+              : listTask.length + " Item Left👍"}</p></div>  
+              <button onClick={deleteAll}>Borrar Todo</button>  
         </div>
         
   );
-};
-
+            };
 export default TodoList;
-
